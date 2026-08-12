81d2839859891b2f52b8e755527f44ed6ced067e
Klaus Harbig <klausharbig@gmail.com>
2026-08-12 20:40:12 +0200

    new card version

--- custom_components/navimower_zone_scheduler/www/navimow-zone-interval-card.js
+++ custom_components/navimower_zone_scheduler/www/navimow-zone-interval-card.js
@@ -74,7 +74,7 @@
  *   end: "20:00"                      # optional, preview/save window end
  */
 
-const CARD_VERSION = "1.3.7";
+const CARD_VERSION = "1.3.8";
 
 class NavimowZoneIntervalCard extends HTMLElement {
   setConfig(config) {
